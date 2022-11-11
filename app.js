4585b2a03890c16c47a61edd42b8b106f5098dde
Gabriele Valenza <g.valenza97@gmail.com>
2022-11-11 22:56:02 +0100

    remove unnecessary files and correct automatic files exposure logic that doesn't work

--- app.js
+++ app.js
@@ -8,36 +8,39 @@ const directoryPath = path.join(__dirname, 'mocks');
 
 
 fs.watch('./mocks', (eventType, filename) => {
-    console.log(`${eventType}: ${filename}`);
+  console.log(`${eventType}: ${filename}`);
+  exponeFiles();
 })
 
 
 const exponeFiles = function () {
-    fs.readdir(directoryPath, function (err, files) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-        files.forEach(function (file) {
-            console.log(file);
-            app.get('/' + file.split('.')[0], (req, res) => {
-                res.sendFile('mocks/' + file, {root: __dirname});
-            });
-        });
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+      console.log(file);
+      app.get('/' + file.split('.')[0], (req, res) => {
+        res.sendFile('mocks/' + file, {root: __dirname});
+      });
     });
+    console.log('-----------');
+  });
 }
 
-exponeFiles();
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
+  res.sendFile('index.html', {root: __dirname});
 });
 
 app.get('/example', (req, res) => {
-    res.sendFile('example.json', {root: __dirname});
+  res.sendFile('example.json', {root: __dirname});
 });
 
 app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+  console.log(`Now listening on http://localhost:${port}`);
+  console.log('-----------');
+  exponeFiles();
 });
 
 
