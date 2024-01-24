beccb38cec978139b11cfc2e7bb07359df0d41c9
Simone Colnaghi <simocolna93@gmail.com>
2024-01-24 09:32:35 +0100

    Wip on reading subfolders

--- app.js
+++ app.js
@@ -4,27 +4,47 @@ const express = require('express');
 const app = express();
 
 const port = 8099;
-const directoryPath = path.join(__dirname, 'mocks');
-
+const mainDirPath = path.join(__dirname, 'mocks');
 
 fs.watch('./mocks', (eventType, filename) => {
   console.log(`${eventType}: ${filename}`);
-  exponeFiles();
+  exposeFile(mainDirPath);
 })
 
-
-const exponeFiles = function () {
-  fs.readdir(directoryPath, function (err, files) {
+const exposeFile = (currentDirPath) => {
+  fs.readdir(currentDirPath, (err, files) => {
     if (err) {
       return console.log('Unable to scan directory: ' + err);
     }
-    files.forEach(function (file) {
-      console.log(file);
-      app.get('/' + file.split('.')[0], (req, res) => {
-        res.sendFile('mocks/' + file, {root: __dirname});
+    
+    const jsonList = files.filter(f => f.endsWith(".json"));
+    
+    jsonList.forEach((json) => {
+      app.get('/' + json.split('.')[0], (req, res) => {
+        console.log(`Serving path ${currentDirPath}/${json}`);
+        res.sendFile(json, {root: currentDirPath});
       });
     });
-    console.log('-----------');
+    
+    const dirList = files.filter(f => !f.endsWith(".json"));
+    
+    dirList.forEach((dirName) => {
+      if (dirName === '.DS_Store') { // create a list of folders to ignore
+        return console.debug(`Folder with name ${dirName} ignored`);
+      }
+      
+      const dirFullPath = path.join(currentDirPath, dirName);
+      
+      fs.stat(dirFullPath, (err, stats) => {
+        if (err) {
+          return console.error('Error getting file stats:', err);
+        }
+        
+        if (stats.isDirectory()) {
+          exposeFile(dirFullPath); // Recursive call for subdirectories
+        }
+      });
+    });
   });
 }
 
@@ -33,14 +53,14 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname});
 });
 
-app.get('/example', (req, res) => {
+/*app.get('/example', (req, res) => {
+  // maybe should add mocks/ dir
   res.sendFile('example.json', {root: __dirname});
-});
+});*/
 
 app.listen(port, () => {
   console.log(`Now listening on http://localhost:${port}`);
-  console.log('-----------');
-  exponeFiles();
+  exposeFile(mainDirPath);
 });
 
 
